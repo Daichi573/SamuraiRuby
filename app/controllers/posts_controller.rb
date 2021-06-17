ffb16322e8290b9a126c2a65d277a01053755cdb
EC2 Default User <ec2-user@ip-172-31-0-215.ap-northeast-1.compute.internal>
2021-06-17 14:31:09 +0000

    fix flash message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,10 +25,10 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
